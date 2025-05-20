443427d2dfd91e0a691d1e19183ce8d498f15bb3
Mohtasham Murshid <mohtashammurshid@gmail.com>
2025-05-20 14:46:14 +0800

    Refactor main function to accept a list of numbers and identify duplicates

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-def main():
-    for i in range(1, 11):
-        print(f"Hello World {i}!")
+def main(nums):
+    temparr = []
+    duplicates_found = False
+    
+    for num in range(nums.__len__()):
+        if num+1 < nums.__len__():
+            if nums[num] == nums[num+1]:
+                if not duplicates_found:
+                    print("Duplicate found")
+                    duplicates_found = True
+            temparr.append(nums[num])
+            print("new array", temparr)
+            
+    if not duplicates_found:
+        print("No duplicates found")
+    print("End of program")
 
 
 if __name__ == "__main__":
-    main()
+    main(nums=[1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 10])
+# This code is a simple Python script that defines a main function which takes a list of numbers as input.
 
 
